347710452f576e29e54067e44d5cd0111650393c
Fabian <jobeliusf@gmail.com>
2025-10-30 14:00:55 +0100

    sorts

--- quicksort/quicksort.go
+++ quicksort/quicksort.go
@@ -4,8 +4,17 @@ package mergesort
 // (dem Pivot). Alle Elemente kleiner oder gleich dem Pivot werden nach links,
 // alle größeren nach rechts einsortiert. Gibt die neue Position des Pivotelements zurück.
 func Partition(arr []int) int {
-	// TODO
-	return 0
+	links := []int{}
+	rechts := []int{}
+	for i := 1; i < len(arr); i++ {
+		if arr[i] >= arr[0] {
+			rechts = append(rechts, arr[i])
+		} else {
+			links = append(links, arr[i])
+		}
+	}
+
+	return len(links)
 }
 
 // QuickSort sortiert die übergebene Liste mittels des Quick-Sort-Algorithmus.
